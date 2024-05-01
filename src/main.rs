e8488f29d1d3636387d835859c723767829b2320
BEnh3N <9805092-BEnh3N@users.noreply.gitlab.com>
2024-05-01 10:56:06 -0700

    added window resizing

--- src/main.rs
+++ src/main.rs
@@ -5,31 +5,29 @@ use raylib::prelude::*;
 use sorting::algorithms::bubble::bubble_sort;
 use sorting::algorithms::merge::merge_sort;
 
-const WIDTH: i32 = 1024;
-const HEIGHT: i32 = 512;
-const LINE_WIDTH: i32 = 2;
-
-const NUM_ITEMS: i32 = WIDTH / LINE_WIDTH;
-const LINE_MUL: i32 = HEIGHT / NUM_ITEMS;
-
 fn main() {
+    let mut state = State::new(512, 1024, 512);
+
     let (mut handle, thread) = init()
-        .size(WIDTH, HEIGHT)
+        .size(state.width, state.height)
         .title("Sorting Algorithms")
         .resizable()
         .build();
 
-    let mut state = State::new(NUM_ITEMS as u32);
-
     while !handle.window_should_close() {
+        // Change scaling if window resized
+        if handle.is_window_resized() {
+            state.update_size(handle.get_screen_width(), handle.get_screen_height())
+        }
+
         let mut draw = handle.begin_drawing(&thread);
         draw.clear_background(Color::BLACK);
 
         // Draw array to screen
         let mut x = 0;
         for value in &state.array {
-            draw.draw_rectangle(x, HEIGHT - (*value as i32 * LINE_MUL), LINE_WIDTH, HEIGHT, Color::WHITE);
-            x += LINE_WIDTH
+            draw.draw_rectangle(x, state.height - (*value as i32 * state.line_mul), state.line_width, state.height, Color::WHITE);
+            x += state.line_width
         }
 
         // GUI
@@ -58,18 +56,35 @@ fn main() {
 
 struct State {
     array: Vec<u32>,
+    size: u32,
+    width: i32,
+    height: i32,
+    line_width: i32,
+    line_mul: i32,
 }
 
 impl State {
-    fn new(size: u32) -> State {
+    fn new(size: u32, width: i32, height: i32) -> State {
         let mut array = (1..=size).collect::<Vec<u32>>();
         array.shuffle(&mut thread_rng());
         State {
             array,
+            size,
+            width,
+            height,
+            line_width: width / size as i32,
+            line_mul: height / size as i32,
         }
     }
 
     fn shuffle(&mut self) {
         self.array.shuffle(&mut thread_rng());
+    }
+
+    fn update_size(&mut self, width: i32, height: i32) {
+        self.width = width;
+        self.height = height;
+        self.line_width = width / self.size as i32;
+        self.line_mul = height / self.size as i32;
     }
 }
